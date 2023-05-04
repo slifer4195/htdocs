1a0a4bb5a20a91343434c60bccd7c1ff15b0e997
ajodutola <96886829+ajodutola@users.noreply.github.com>
2023-05-04 15:57:46 -0500

    flight implemented for Admin

---
+++ main/includes/flightAdminDelete.inc.php
@@ -0,0 +1,27 @@
+<?php
+session_start();
+
+$serverName = "localhost";
+$dBUsername = "root";
+$dBPassword = "";
+$dBName = "AnyWhere";
+
+$conn = mysqli_connect($serverName, $dBUsername, $dBPassword, $dBName);
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+if (isset($_GET['deleteid'])) {
+    $id = $_GET['deleteid'];
+
+    $sql = "DELETE FROM Flights WHERE FlighID = 'deleteid'";
+    $result = mysqli_query($conn, $sql);
+
+    if ($result) {
+        header("location: ../pages/FlightAdmin.php");
+        exit();
+    } else {
+        die(mysqli_error($conn));
+    }
+}
