7c253ea97775963213964405a496b223a65ad471
Sung Rung Yoo <slifer4195@gmail.com>
2023-05-04 21:33:47 -0500

    foreign key deletion

--- main/includes/delete_location.php
+++ main/includes/delete_location.php
@@ -11,12 +11,50 @@ if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
+$sql2 = "SELECT * FROM Activity";
+ 
+// Execute the query and store the result in a variable
+$result2 = mysqli_query($conn, $sql2);
+// $result2 = mysqli_query($conn, $sql2);
+
+// Check if there are any rows in the result
+if (mysqli_num_rows($result2) >= 0) {
+  // Initialize an empty array to store the data
+  $data2 = array();
+
+  // Loop through the result and store each row in the data array
+  while ($row2 = mysqli_fetch_assoc($result2)) {
+    $data2[] = $row2;
+  }
+} else {
+  echo "No data found.";
+}
+
 // Check if the locationId was passed in the form submission
 if (isset($_POST['id'])) {
   // Retrieve the locationId from the $_POST array
   $locationId = $_POST['id'];
   echo "The location ID is: " . $locationId;
+
+  foreach ($data2 as $row2){
+    
+    if ($row2["LocationID"] == $locationId){
+      $sql2 = "DELETE FROM Activity WHERE LocationID=$locationId";
+      if ($conn->query($sql2) === TRUE) {
+        // Redirect the user to a confirmation page or the previous page
+        echo "";
+      } else {
+        echo "Error deleting record: " . $conn->error;
+      }
+    
+    }
+  }
+
+
+
   $sql = "DELETE FROM Location WHERE id=$locationId";
+
+  
 
   if ($conn->query($sql) === TRUE) {
     // Redirect the user to a confirmation page or the previous page
@@ -39,3 +77,8 @@ header("Location: ../pages/Location.php");
   // ...
 }
 ?>
+
+
+
+
+
